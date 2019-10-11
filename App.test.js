88ca8004c072f4fae2fc0916d031ca8713fa1d28
Eduardo Braga <eduardo.braga@delfosim.com>
2019-10-11 13:28:03 -0300

    feature: made render test for app root. Need to made one for each component and them improve tests with more cases than just render

---
+++ App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '~/App';
+
+describe('App (root)', () => {
+    test('Render?', () => {
+        const component = shallow(
+            <App />
+        );
+
+        expect(component).toMatchSnapshot();
+    });
+});
